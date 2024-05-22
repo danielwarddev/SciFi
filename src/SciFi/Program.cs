ec93f51832e374b82366a9e4bb7a54b34697ae9c
Daniel Ward <dward256@hotmail.com>
2024-05-22 17:05:49 -0500

    Initial commit

---
+++ src/SciFi/Program.cs
@@ -0,0 +1,15 @@
+﻿using Amazon.CDK;
+
+namespace SciFi
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new SciFiStack(app, "SciFiStack");
+
+            app.Synth();
+        }
+    }
+}
